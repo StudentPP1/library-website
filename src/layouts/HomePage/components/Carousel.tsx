eebcc606910d1b3e7958e9d6fce6aa741aaadeb6
StudentPP1 <mishamakytonin@gmail.com>
2024-09-30 10:39:52 +0300

    update headers

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -9,18 +9,15 @@ export const Carousel = () => {
     const [books, setBooks] = useState<BookModel[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
-
+  
     useEffect(() => {
         const fetchBooks = async () => {
             const baseUrl: string = `${REACT_APP_API}/api/books`
             const url: string = `${baseUrl}?page=0&size=9`;
             const response = await fetch(url, {
                 headers: {
-                    method: "GET",
-                    credentials: 'include',
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Credentials': 'true'
+                    'Content-Type': "application/json",
+                    'Access-Control-Allow-Origin' : '*',
                 }
             });
 
